a1724f785a3a2c7b41b86d8a3995a82dabdc3aa8
B!guU <biguueditz@gmail.com>
2026-03-03 20:46:16 +0530

    Add WinkSub2.js with subscription data structure

---
+++ WinkSub2.js
@@ -0,0 +1,37 @@
+{
+  "code": 0,
+  "error_code": "00000",
+  "message": "success",
+  "data": {
+    "account_type": 1,
+    "account_id": "1197334579",
+    "is_vip": true,
+    "use_vip": true,
+    "limit_type": 0,
+    "valid_time": "1546992000000",
+    "invalid_time": "32662195199000",
+    "expire_days": 999,
+    "have_valid_contract": false,
+    "show_renew_flag": false,
+    "show_renew_flag_abroad": true,
+    "sub_type": 2,
+    "sub_type_name": "续期",
+    "active_product_id": "0",
+    "active_order_id": "0",
+    "active_sub_order_id": "0",
+    "active_sub_type": 0,
+    "active_sub_type_name": "",
+    "in_trial_period": false,
+    "in_grace_period": false,
+    "derive_type": 1,
+    "derive_type_name": "普通会员",
+    "membership": {
+      "id": "4",
+      "display_name": "Wink会员",
+      "level": 1,
+      "level_name": "普通会员"
+    }
+  },
+  "request_id": "988049c53e0f4829b4f89bfff93ed253",
+  "success": true
+}
