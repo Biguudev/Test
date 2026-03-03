1502f10e03835e0fa96fd96d065ba3c404e3ac4c
B!guU <biguueditz@gmail.com>
2026-03-03 20:02:46 +0530

    Add files via upload

---
+++ balance.js
@@ -0,0 +1,14 @@
+{
+  "code": 0,
+  "error_code": "00000",
+  "message": "success",
+  "data": {
+    "meiye_balance": "9999",
+    "total_amount": "9999",
+    "meiye_freeze_amount": "0",
+    "my_credits": "My Credits",
+    "meidou_balance": "0"
+  },
+  "request_id": "15d8b430fcec49a8bbd08418452a6ab6",
+  "success": true
+}
