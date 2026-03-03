1e98399d7390dc5185182d4d310dbee0cd57eb1f
B!guU <biguueditz@gmail.com>
2026-03-03 20:36:17 +0530

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,51 @@
+{
+  "reqid": "3e25185d-d026-4514-932a-de768183a73e",
+  "code": 0,
+  "data": {
+    "nation_code": "LK",
+    "ip_nation_code": "",
+    "subscribe_rich_tip": {
+      "home": {
+        "id": 667,
+        "scheme": "mtwink://webview?hideHeader=true&isDarkMode=false&url=https%3A%2F%2Ftitan-h5.meitu.com%2Fapp%2Fsubscription%2Fvip-home%2Findex.html%3Fsubscription_support%3D1%26scene%3Dwink.vipcenter.scene%26appId%3D6829803307010000000",
+        "display_type": 1,
+        "put_type": 0,
+        "carousel": null,
+        "cover_pic": "",
+        "width": 120,
+        "height": 120,
+        "badge": "",
+        "badge_width": 0,
+        "badge_height": 0,
+        "countdown": 0,
+        "countdown_id": 0,
+        "countdown_color": "",
+        "countdown_bg_color": "",
+        "experiment_type": 0
+      },
+      "mine": {
+        "id": 666,
+        "scheme": "mtwink://webview?hideHeader=true&isDarkMode=false&url=https%3A%2F%2Ftitan-h5.meitu.com%2Fapp%2Fsubscription%2Fvip-home%2Findex.html%3Fsubscription_support%3D1%26scene%3Dwink.vipcenter.scene%26appId%3D6829803307010000000",
+        "display_type": 2,
+        "put_type": 0,
+        "carousel": null,
+        "cover_pic": "https://wink-material.meitudata.com/5d2j23PZDMz9Abe8.png",
+        "width": 0,
+        "height": 0,
+        "badge": "",
+        "badge_width": 0,
+        "badge_height": 0,
+        "countdown": 0,
+        "countdown_id": 0,
+        "countdown_color": "",
+        "countdown_bg_color": "",
+        "experiment_type": 0
+      }
+    },
+    "service_message_list": {
+      "item_list": null,
+      "last_created_at": 0,
+      "current_time": 1772434437
+    }
+  }
+}
